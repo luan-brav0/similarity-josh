f58d0966253b0eb67f92c63267d55ee6a50e3e6d
Luan Bravo <luan.melo.bravo10@gmail.com>
2023-04-05 14:57:07 -0300

    Heading and Paragraph reusable comp created

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,9 +1,10 @@
-import React, { HTMLAttributes } from 'react'
-import { cva } from 'class-variance-authority'
+import React, { HTMLAttributes, forwardRef } from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+import { cn } from '@/app/lib/utils';
 
-interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement>
+    , VariantProps<typeof paragraphVariants> { };
 
-}
 const paragraphVariants = cva(
     "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
     {
@@ -17,12 +18,15 @@ const paragraphVariants = cva(
             size: 'default',
         }
     }
-)
+);
 
-const Paragraph: React.FC = ({ }: ParagraphProps) => {
-    return (
-        <div>Paragraph</div>
-    )
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(({ className, size, children, ...props }, ref) => {
+    return (<p ref={ref} {...props} className={cn(paragraphVariants({ size, className }))}>
+        {children}
+    </p>
+    );
 }
+);
+Paragraph.displayName = "Paragraph";
 
-export default Paragraph
+export default Paragraph;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-
+import Paragraph from './components/ui/Paragraph'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="bg-red-700">
-      hello there
+      <Paragraph size='sm'/>
     </main>
   )
 }
